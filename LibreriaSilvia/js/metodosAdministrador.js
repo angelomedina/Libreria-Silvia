c86b4fcb1cd75ef5da2a5482283e90894b0e8287
Wilfred <wilcr20@gmail.com>
2018-06-08 09:10:35 -0600

    Bandejas Listas
    Bandeja de solicitudes realizadas y  boton listo con ID de solicitud asociado

--- LibreriaSilvia/js/metodosAdministrador.js
+++ LibreriaSilvia/js/metodosAdministrador.js
@@ -1,6 +1,7 @@
 google.charts.load("current", {packages:["corechart"]});
 google.charts.setOnLoadCallback(drawChart);
 
+var solicitudesJson;
 
 function activarVale() {
 
@@ -39,6 +40,7 @@ function activarVale() {
     xhttp.send();
 }
 
+
 function mostrarSolicitudes() {
 
     var xhttp = new XMLHttpRequest();
@@ -47,10 +49,59 @@ function mostrarSolicitudes() {
         if (this.readyState == 4 && this.status == 200) {
             if(this.statusText== "OK" && this.status == 200) {
 
-                var response = this.response;
-                var json     = JSON.parse(response);
+                //var response = this.response;
+                //var json     = JSON.parse(response);
 
-                verificaEstadoSolicitud(json);
+                solicitudesJson= [
+                    {
+                        "nombre":"Wilfred",
+                        "primerApellido":"Alvarado",
+                        "segundoApellido":"Maltez",
+                        "cantidad":1,
+                        "color":"B",
+                        "pagina":"12-45",
+                        "correo":"wilcr20@gmail.com",
+                        "telefono":"6098-0967",
+                        "fecha":{
+                            "date":"23-04-2018 12:34:01.123400"
+                        },
+                        "estado":"T",
+                        "id":1
+                    },
+                    {
+                        "nombre":"Carmen",
+                        "primerApellido":"Lyra",
+                        "segundoApellido":"Lyra",
+                        "cantidad":2,
+                        "color":"N",
+                        "pagina":"145-150",
+                        "correo":"lara@gmail.com",
+                        "telefono":"6100-0900",
+                        "fecha":{
+                            "date":"21-04-2018 10:34:01.123400"
+                        },
+                        "estado":"T",
+                        "id":2
+                    },
+
+                    {
+                        "nombre":"Lucia",
+                        "primerApellido":"Lyra",
+                        "segundoApellido":"Lyra",
+                        "cantidad":2,
+                        "color":"N",
+                        "pagina":"145-150",
+                        "correo":"lara@gamil.com",
+                        "telefono":"6100-0900",
+                        "fecha":{
+                            "date":"11-04-2018 18:00:01.123400"
+                        },
+                        "estado":"T",
+                        "id":3
+                    }
+                ]
+
+                verificaEstadoSolicitud(solicitudesJson);
             }
             else{console.log(this.statusText, this.status)}
         }
@@ -193,6 +244,8 @@ function agregagrElementosSelect(fecha) {
             select.add(option);
 }
 
+
+
 function rellenarTabla(solicitudesJson) {
 
     var tabla = document.getElementById("tablaSolicitudes");
@@ -202,6 +255,7 @@ function rellenarTabla(solicitudesJson) {
     for( x= 0; x <solicitudesJson.length; x++) {
         // se generan las 8 celdas respectivas por cada solicitud
         var Nrow = tabla.insertRow(numeroSolicitud);
+
         var cell1 = Nrow.insertCell(0);
         var cell2 = Nrow.insertCell(1);
         var cell3 = Nrow.insertCell(2);
@@ -211,6 +265,8 @@ function rellenarTabla(solicitudesJson) {
         var cell7 = Nrow.insertCell(6);
         var cell8 = Nrow.insertCell(7);
         var cell9= Nrow.insertCell(8);
+        var cell10= Nrow.insertCell(9);
+
         numeroSolicitud++;
 
         var paginas= JSON.stringify(solicitudesJson[x]["pagina"]); // ejemplos de dato: "1-45", "23-87"
@@ -222,6 +278,7 @@ function rellenarTabla(solicitudesJson) {
             paginaInicio += paginas[char]; // a traves de ciclo hasta topar con '-'
             char++;
         }
+
         char++;
         while (paginas[char] !='"'){ // se halla la pagina final
             paginaFin += paginas[char]; // a traves de ciclo hasta topar con "
@@ -236,9 +293,69 @@ function rellenarTabla(solicitudesJson) {
         var telefono = JSON.stringify(solicitudesJson[x]["telefono"]).replace(/['"]+/g, '');
         var fechaPura= fechaJson[0].replace(/['"]+/g, '');
 
+        pCorreo= correo.toString();
+        pNombre= nombre.toString();
+        pFecha = fechaPura.toString();
+
+        cell1.innerHTML= JSON.stringify(solicitudesJson[x]["id"]);
+        cell2.innerHTML = nombre+" "+apell1 +" "+ apell2;
+        cell3.innerHTML = JSON.stringify(solicitudesJson[x]["color"]).replace(/['"]+/g, '');
+        cell4.innerHTML = JSON.stringify(solicitudesJson[x]["cantidad"]);
+        cell5.innerHTML = paginaInicio;
+        cell6.innerHTML = paginaFin;
+        cell7.innerHTML= correo;
+        cell8.innerHTML = telefono;
+        cell9.innerHTML = fechaJson[0].replace(/['"]+/g, '');
+        cell10.innerHTML = '<a class="btn2" onclick="abrirDocumento()">Abrir</a>  <br><br>  <a class="btn2" onclick="pedidoListo(this)">Listo</a>'
+
+    }
+}
 
 
-        pid    = solicitudesJson[x]["id"];
+function rellenarTablaSolicitudesListas(solicitudesJson) {
+
+    var tabla = document.getElementById("tablaSolicitudesLista");
+
+    var numeroSolicitud=1; // para lograr mostrar en orden de llegada las solicitudes
+
+    for( x= 0; x <solicitudesJson.length; x++) {
+        // se generan las 8 celdas respectivas por cada solicitud
+        var Nrow = tabla.insertRow(numeroSolicitud);
+        var cell1 = Nrow.insertCell(0);
+        var cell2 = Nrow.insertCell(1);
+        var cell3 = Nrow.insertCell(2);
+        var cell4 = Nrow.insertCell(3);
+        var cell5 = Nrow.insertCell(4);
+        var cell6 = Nrow.insertCell(5);
+        var cell7 = Nrow.insertCell(6);
+        var cell8 = Nrow.insertCell(7);
+
+        numeroSolicitud++;
+
+        var paginas= JSON.stringify(solicitudesJson[x]["pagina"]); // ejemplos de dato: "1-45", "23-87"
+        var paginaInicio ="";  // variables para almacenar
+        var paginaFin ="";  // las paginas
+
+        var char = 1; //indice
+        while (paginas[char] != "-") { // se halla la pagina inicial
+            paginaInicio += paginas[char]; // a traves de ciclo hasta topar con '-'
+            char++;
+        }
+
+        char++;
+        while (paginas[char] !='"'){ // se halla la pagina final
+            paginaFin += paginas[char]; // a traves de ciclo hasta topar con "
+            char++;
+        }
+        // se crean variables para castearlas adecaudamaente
+        var nombre = JSON.stringify(solicitudesJson[x]["nombre"]).replace(/['"]+/g, '');
+        var apell1= JSON.stringify(solicitudesJson[x]["primerApellido"]).replace(/['"]+/g, '');
+        var apell2= JSON.stringify(solicitudesJson[x]["segundoApellido"]).replace(/['"]+/g, '');
+        var fechaJson = JSON.stringify(solicitudesJson[x]["fecha"]["date"]).split(".");
+        var correo=  JSON.stringify(solicitudesJson[x]["correo"]).replace(/['"]+/g, '');
+        var telefono = JSON.stringify(solicitudesJson[x]["telefono"]).replace(/['"]+/g, '');
+        var fechaPura= fechaJson[0].replace(/['"]+/g, '');
+
         pCorreo= correo.toString();
         pNombre= nombre.toString();
         pFecha = fechaPura.toString();
@@ -251,21 +368,26 @@ function rellenarTabla(solicitudesJson) {
         cell6.innerHTML= correo;
         cell7.innerHTML = telefono;
         cell8.innerHTML = fechaJson[0].replace(/['"]+/g, '');
-        cell9.innerHTML = '<a class="btn2" onclick="abrirDocumento()">Abrir</a>  <br><br>  <a class="btn2" onclick="pedidoListo(this,pid)">Listo</a>'
+
     }
 }
 
-function pedidoListo(row,id) {
+
+
+
+function pedidoListo(row) {
 
     var i = row.parentNode.parentNode.rowIndex;  // se consigue el indice de la fila a utilizar para conseguir los datos
 
-    var nombre= document.getElementById("tablaSolicitudes").rows[i].cells[0].innerText;
-    var correo= document.getElementById("tablaSolicitudes").rows[i].cells[5].innerText;
+    var nombre= document.getElementById("tablaSolicitudes").rows[i].cells[1].innerText;
+    var correo= document.getElementById("tablaSolicitudes").rows[i].cells[6].innerText;
     var fecha = document.getElementById("tablaSolicitudes").rows[i].cells[7].innerText;
+    var id= document.getElementById("tablaSolicitudes").rows[i].cells[0].innerText;
+
+    console.log("ID: "+ id + ","+nombre+","+fecha);
 
     var service_id = "default_service";
     var template_id = "pedidolisto";
-
 
     document.getElementById("tablaSolicitudes").deleteRow(i);  // elimina fila de la tabla de solicitudes
 
@@ -274,7 +396,7 @@ function pedidoListo(row,id) {
     emailjs.send(service_id,template_id,params)
         .then(function(){
 
-            estadoSolicitud(id);
+            estadoSolicitud(id);  // recibe ID de solicitud .....
 
             swal({
                 title: "Correo de confimación listo!",
@@ -332,6 +454,8 @@ function estadoSolicitud(id){
     xhttp.send();
 }
 
+
+
 function mostrarSolicitudesListas() {
 
     var xhttp = new XMLHttpRequest();
@@ -340,7 +464,75 @@ function mostrarSolicitudesListas() {
         if (this.readyState == 4 && this.status == 200) {
             if(this.statusText== "OK" && this.status == 200) {
 
-                console.log(this.response);
+                //console.log(this.response);
+                solicitudesJson= [
+                    {
+                        "nombre":"Wilfred",
+                        "primerApellido":"Alvarado",
+                        "segundoApellido":"Maltez",
+                        "cantidad":1,
+                        "color":"B",
+                        "pagina":"12-45",
+                        "correo":"wilcr20@gmail.com",
+                        "telefono":"6098-0967",
+                        "fecha":{
+                            "date":"23-04-2018 12:34:01.123400"
+                        },
+                        "estado":"F",
+                        "id":1
+                    },
+                    {
+                        "nombre":"Carmen",
+                        "primerApellido":"Lyra",
+                        "segundoApellido":"Lyra",
+                        "cantidad":2,
+                        "color":"N",
+                        "pagina":"145-150",
+                        "correo":"lara@gmail.com",
+                        "telefono":"6100-0900",
+                        "fecha":{
+                            "date":"21-04-2018 10:34:01.123400"
+                        },
+                        "estado":"F",
+                        "id":2
+                    },
+
+                    {
+                        "nombre":"Lucia",
+                        "primerApellido":"Lyra",
+                        "segundoApellido":"Lyra",
+                        "cantidad":2,
+                        "color":"N",
+                        "pagina":"145-150",
+                        "correo":"lara@gmmil.com",
+                        "telefono":"6100-0900",
+                        "fecha":{
+                            "date":"11-04-2018 18:00:01.123400"
+                        },
+                        "estado":"F",
+                        "id":3
+                    },
+
+
+                    {
+                        "nombre":"Marco",
+                        "primerApellido":"Esquivel",
+                        "segundoApellido":"Vargas",
+                        "cantidad":1,
+                        "color":"N",
+                        "pagina":"115-190",
+                        "correo":"wilrex@gmmil.com",
+                        "telefono":"6101-9940",
+                        "fecha":{
+                            "date":"18-06-2018 18:00:01.123400"
+                        },
+                        "estado":"F",
+                        "id":4
+                    }
+                ]
+
+                rellenarTablaSolicitudesListas(solicitudesJson);
+
             }
             else{console.log(this.statusText, this.status)}
         }
